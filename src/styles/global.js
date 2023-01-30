8f9d5cff88cb4d3685cd8ad5fc0add1e59557482
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2023-01-30 07:51:55 -0300

    feat: text class

--- src/styles/global.js
+++ src/styles/global.js
@@ -50,4 +50,120 @@ export default createGlobalStyle`
   button:hover, a:hover {
     filter: brightness(0.9);
   }
+
+  .poppins-100-medium {
+    font-family: 'Poppins', sans-serif;
+    font-weight: medium;
+    font-size: 0.875rem;
+    line-height: 150%;
+    text-decoration: none;
+  }
+
+  .poppins-200-medium {
+    font-family: 'Poppins', sans-serif;
+    font-weight: medium;
+    font-size: 1.25rem;
+    line-height: 160%;
+    text-decoration: none;
+  }
+
+  .poppins-300-bold {
+    font-family: 'Poppins', sans-serif;
+    font-weight: bold;
+    font-size: 1.5rem;
+    line-height: 140%;
+    text-decoration: none;
+  }
+
+  .poppins-300-regular {
+    font-family: 'Poppins', sans-serif;
+    font-weight: regular;
+    font-size: 1.5rem;
+    line-height: 140%;
+    text-decoration: none;
+  }
+
+  .poppins-400-medium {
+    font-family: 'Poppins', sans-serif;
+    font-weight: medium;
+    font-size: 2rem;
+    line-height: 140%;
+    text-decoration: none;
+  }
+
+  .poppins-500-medium {
+    font-family: 'Poppins', sans-serif;
+    font-weight: medium;
+    font-size: 2.5rem;
+    line-height: 140%;
+    text-decoration: none;
+  }
+
+  .roboto-200-regular {
+    font-family: 'Roboto', sans-serif;
+    font-weight: regular;
+    font-size: 0.75rem;
+    line-height: 160%;
+    text-decoration: none;
+  }
+
+  .roboto-300-regular {
+    font-family: 'Roboto', sans-serif;
+    font-weight: regular;
+    font-size: 0.875rem;
+    line-height: 160%;
+    text-decoration: none;
+  }
+
+  .roboto-300-bold {
+    font-family: 'Roboto', sans-serif;
+    font-weight: bold;
+    font-size: 0.875rem;
+    line-height: 160%;
+    text-decoration: none;
+  } 
+
+
+  .roboto-400-spaced {
+    font-family: 'Roboto', sans-serif;
+    font-weight: regular;
+    font-size: 1rem;
+    line-height: 160%;
+    text-decoration: none;
+  }  
+
+  .roboto-400-regular {
+    font-family: 'Roboto', sans-serif;
+    font-weight: regular;
+    font-size: 1rem;
+    line-height: 100%;
+    text-decoration: none;
+  }  
+
+  .roboto-500-bold {
+    font-family: 'Roboto', sans-serif;
+    font-weight: bold;
+    font-size: 1.25rem;
+    line-height: 160%;
+    text-decoration: none;
+  }
+
+  .roboto-700-regular {
+    font-family: 'Roboto', sans-serif;
+    font-weight: regular;
+    font-size: 2rem;
+    line-height: 160%; 
+    text-decoration: none;
+  }
+
+  .roboto-900-bold {
+    font-family: 'Roboto', sans-serif;
+    font-weight: bold;
+    font-size: 2.625rem;
+    line-height: automatic; 
+    text-decoration: none;
+  }
+  
+  
+  
 `;

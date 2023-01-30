8006d03882258cf19c0f2cd1a7a3ccdf1d04db73
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2023-01-30 07:05:14 -0300

    feat: token colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,20 +1,34 @@
 export default {
   COLORS: {
-    BACKGROUND_900: "#262529",
-    BACKGROUND_800: "#1C1B1E",
-    BACKGROUND_700: "#3E3B47",
+    LIGHT_100: "hsla(0,0%,100%,1)",
+    LIGHT_200: "hsla(39,100%,97%,1)",
+    LIGHT_300: "hsla(240,9%,89%,1)",
+    LIGHT_400: "hsla(240,7%,78%,1)",
+    LIGHT_500: "hsla(240,6%,51%,1)",
+    LIGHT_600: "hsla(204,2%,47%,1)",
+    LIGHT_700: "hsla(201,10%,34%,1)",
 
-    WHITE: "#F4EDE8",
-    BLACK: "#0D0C0F",
+    DARK_100: "hsla(192, 100%, 1%, 1)",
+    DARK_200: "hsla(198, 100%, 2%, 1)",
+    DARK_300: "hsla(210, 100%, 1%, 1)",
+    DARK_400: "hsla(199, 100%, 3%, 1)",
+    DARK_500: "hsla(198, 100%, 4%, 1)",
+    DARK_600: "hsla(200, 100%, 5%, 1)",
+    DARK_700: "hsla(199, 100%, 5%, 1)",
+    DARK_800: "hsla(201, 35%, 8%, 1)",
+    DARK_900: "hsla(200, 48%, 10%, 1)",
+    DARK_1000: "hsla(201, 22%, 13%, 1)",
 
-    PINK: "#FF859B",
-    PINK_100: "#FF859B0D",
+    GRADIENT_100: "linear-gradient(90deg, hsla(200, 100%, 3%, 0.27) 0%, hsla(200, 100%, 3%, 1) 100%)",
+    GRADIENT_200: "linear-gradient(180deg, hsla(198, 61%, 9%, 1) 0%, hsla(200, 100%, 5%, 1) 100%)",
 
-
-
-    GRAY_100: "#999591",
-    GRAY_300: "#666360",
-    GRAY_400: "#312E38",
-    RED: "#FF002E",
+    TOMATO_100: "hsla(353, 95%, 24%, 1)",
+    TOMATO_200: "hsla(354, 100%, 29%, 1)",
+    TOMATO_300: "hsla(355, 67%, 40%, 1)",
+    TOMATO_400: "hsla(355, 38%, 49%, 1)",
+    
+    CARROT_100: "hsla(32, 96%, 64%, 1)",
+    MINT_100: "hsla(147, 96%, 42%, 1)",
+    CAKE_100: "hsla(195, 91%, 25%, 1)"
   }
 }

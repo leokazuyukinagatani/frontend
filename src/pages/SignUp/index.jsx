fc42fbb7c97f89cfaed450631760a3689756ed4d
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2023-01-30 07:04:41 -0300

    feat: signUp page structure

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles";
+import { Input } from "../../components/Input";
+
+export function SignUp() {
+  return (
+    <Container>
+      <h1>Rocket Foods</h1>
+      <h2>Faça seu login</h2>
+
+    </Container>
+  );
+}

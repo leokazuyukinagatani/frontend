5b056dccc0ef073975e506027037f2f5745c7db8
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2023-02-12 15:51:04 -0300

    feat: signin page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,14 +1,37 @@
-import { Container } from "./styles";
-import { Input } from "../../components/Input";
-import useForm from "../../hooks/useForm";
+import { Form, Container } from './styles'
+
+import { Link } from 'react-router-dom'
+import Logo from '../../assets/logo.svg'
+import { Input } from '../../components/Input'
+import { useState } from 'react'
+import { Button } from '../../components/Button'
 
 export function SignIn() {
-  const email = useForm("email");
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
     <Container>
-      <h1>Rocket Foods</h1>
-      <h2>Faça seu login</h2>
-      <Input id="email" label="Email" type="email" {...email} />
+      <img src={Logo} alt="logo" />
+      <Form>
+        <Input
+          label="Email"
+          type="email"
+          placeholder="Exemplo: exemplo@exemplo.com.br"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <Input
+          label="Password"
+          type="password"
+          placeholder="No mínimo 6 caracteres"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <Button title="Entrar"/>
+        <Link to="/register">Criar uma conta</Link>
+      </Form>
     </Container>
-  );
+  )
 }

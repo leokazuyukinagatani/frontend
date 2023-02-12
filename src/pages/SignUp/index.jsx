5b1c6787a05c14b0acb04f2b650aa266d91a7d29
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2023-02-12 15:51:26 -0300

    feat: signup page

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,12 +1,46 @@
-import { Container } from "./styles";
-import { Input } from "../../components/Input";
+import { Form, Container } from './styles'
+
+import { Link } from 'react-router-dom'
+import Logo from '../../assets/logo.svg'
+import { Input } from '../../components/Input'
+import { useState } from 'react'
+import { Button } from '../../components/Button'
 
 export function SignUp() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
     <Container>
-      <h1>Rocket Foods</h1>
-      <h2>Faça seu login</h2>
+      <img src={Logo} alt="logo" />
+      <Form>
+        <Input
+          label="Seu nome"
+          type="text"
+          placeholder="Exemplo: Maria da Silva"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
+        <Input
+          label="Email"
+          type="email"
+          placeholder="Exemplo: exemplo@exemplo.com.br"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <Input
+          label="Password"
+          type="password"
+          placeholder="No mínimo 6 caracteres"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <Button title="Criar conta" />
+        <Link to="/">Já tenho uma conta</Link>
+      </Form>
     </Container>
-  );
+  )
 }
--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -1,5 +1,27 @@
 import styled from "styled-components"
-export const Container = styled.div`
 
+export const Container = styled.div`
+ background-color: ${({ theme }) => theme.COLORS.DARK_400};
+  height: 100vh;
+  width: 100%;
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  
+
+  img {
+    margin-bottom: 4.75rem;
+  }
+`
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  color: ${({ theme }) => theme.COLORS.LIGHT_400};
 
 `
